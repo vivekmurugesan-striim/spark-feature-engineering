f6d767fbfd4a2ab424613332ab553380ae5988bd
vivekmurugesan-striim <vivek.murugesan@striim.com>
2026-05-26 22:37:07 +0530

    To process only customer and trans features to test.

--- fraud-detection-ml-featureengg/src/main/java/com/test/feature/engg/SparkDriver.java
+++ fraud-detection-ml-featureengg/src/main/java/com/test/feature/engg/SparkDriver.java
@@ -21,6 +21,7 @@ public class SparkDriver {
         // 1. Read Raw Datasets
         //CustomerHeader: ID,ACCOUNT_AGE_DAYS,AGE,CITY,CREDIT_SCORE,FIRST_NAME,
         // LAST_NAME
+        System.out.println("Reading files and creating Spark datasets..");
         Dataset<Row> customers = spark.read().option("header", "true").csv(inputDir + "/customer*.csv");
         //CustemerDeviceHeader: ID,DEVICE_TYPE,OS,STATUS,CUST_ID
         Dataset<Row> customerDevices = spark.read().option("header", "true").csv(inputDir + "/customer_device*.csv");
@@ -31,26 +32,36 @@ public class SparkDriver {
         //FraudTransactionHeader: ID,IS_FRAUD,TRANS_ID
         Dataset<Row> fraudTransactions = spark.read().option("header", "true").csv(inputDir + "/fraud_transaction*.csv");
 
+        System.out.println("Generating feature groups.. for transaction and " +
+                "customers..");
         // 2. Generate Feature Groups
         Dataset<Row> transFeats = FeatureGenerationUtils.generateTransactionFeatures(transactions,
                 customers, merchants, fraudTransactions);
         Dataset<Row> custFeats =
                 FeatureGenerationUtils.generateCustomerFeatures(transactions, customers,  fraudTransactions, customerDevices);
-        Dataset<Row> merchFeats = FeatureGenerationUtils.generateMerchantFeatures(transactions, merchants, fraudTransactions);
+        /*Dataset<Row> merchFeats =
+                FeatureGenerationUtils.generateMerchantFeatures(transactions, merchants, fraudTransactions);
         Dataset<Row> merchCatFeats = FeatureGenerationUtils.generateMerchantCategoryFeatures(transactions, merchants, fraudTransactions);
         Dataset<Row> custMerchFeats = FeatureGenerationUtils.generateCustomerMerchantFeatures(transactions);
-        Dataset<Row> custMerchCatFeats = FeatureGenerationUtils.generateCustomerMerchantCategoryFeatures(transactions, merchants);
+        Dataset<Row> custMerchCatFeats = FeatureGenerationUtils
+        .generateCustomerMerchantCategoryFeatures(transactions, merchants);
+         */
 
+        System.out.println("Persisting transaction and customer feature " +
+                "groups to files.. ");
         // 3. Persist Individual Groups
         transFeats.write().mode("overwrite").option("header", "true").csv(outputDir + "/TransactionFeatures");
         custFeats.write().mode("overwrite").option("header", "true").csv(outputDir + "/CustomerFeatures");
+        /*
         merchFeats.write().mode("overwrite").option("header", "true").csv(outputDir + "/MerchantFeatures");
         merchCatFeats.write().mode("overwrite").option("header", "true").csv(outputDir + "/MerchantCategoryFeatures");
         custMerchFeats.write().mode("overwrite").option("header", "true").csv(outputDir + "/CustomerMerchantFeatures");
-        custMerchCatFeats.write().mode("overwrite").option("header", "true").csv(outputDir + "/CustomerMerchantCategoryFeatures");
+        custMerchCatFeats.write().mode("overwrite").option("header", "true")
+        .csv(outputDir + "/CustomerMerchantCategoryFeatures");
+         */
 
         // 4. Combine for Training Records
-        Dataset<Row> trainingRecords = transFeats
+        /*Dataset<Row> trainingRecords = transFeats
                 .join(custFeats, transFeats.col("CUSTOMER_ID").equalTo(custFeats.col("CF_ID")), "left")
                 .join(merchFeats, transFeats.col("MERCHANT_ID").equalTo(merchFeats.col("MF_ID")), "left")
                 .join(merchCatFeats, transFeats.col("CATEGORY").equalTo(merchCatFeats.col("MCF_CATEGORY")), "left")
@@ -61,7 +72,9 @@ public class SparkDriver {
                         transFeats.col("CUSTOMER_ID").equalTo(custMerchCatFeats.col("CMC_CUSTOMER_ID"))
                                 .and(transFeats.col("CATEGORY").equalTo(custMerchCatFeats.col("CMC_CATEGORY"))), "left");
 
-        trainingRecords.write().mode("overwrite").option("header", "true").csv(outputDir + "/TrainingRecords");
+        trainingRecords.write().mode("overwrite").option("header", "true")
+        .csv(outputDir + "/TrainingRecords");
+         */
 
         spark.stop();
     }
